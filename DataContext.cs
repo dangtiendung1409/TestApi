c7a2ade9ee86d45ae3035e008c17f67561fc5b43
Dũng <114909128+dangtiendung1409@users.noreply.github.com>
2024-04-03 15:10:06 +0700

    Add files via upload

---
+++ DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using TestApi;
+using TestApi.Model;
+
+namespace TestApi
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Todo => Set<Employee>();
+    }
+}
+
